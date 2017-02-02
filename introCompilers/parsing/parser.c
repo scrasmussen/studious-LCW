fb50137e02c3dea8fbd1affe9bf7e365ef0dcac0
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-02-02 15:10:55 -0800

    Updated to read from file

--- introCompilers/parsing/parser.c
+++ introCompilers/parsing/parser.c
@@ -13,6 +13,27 @@ extern int lineNum;
 extern int yy_flex_debug;
 
 int main(int argc, char*argv[]) {
+  // open a file handle to a particular file:
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " QUACK.INPUT.FILE" << std::endl;
+    return 1;
+  }
 
+  FILE *f = fopen(argv[1], "r");
+  // make sure it is valid:
+  if (!f) {
+    std::cout << "Unable to open input file!" << std::endl;
+    return -1;
+  }
+
+  // set flex to read from it instead of defaulting to STDIN:
+  yyin = f;
+
+  std::cout << "Beginning Parsing\n";
+  if (yyparse() == 0)
+    std::cout << "Parsing Successful\n";
+  else
+    std::cout << "Parsing Failed\n";
+  
   std::cout << "Fin" << std::endl;
 }
