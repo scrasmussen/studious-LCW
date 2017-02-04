60422fbfa77e0deb338208a8ab7dbf998a0fc995
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-02-03 20:03:52 -0800

    Initial version

---
+++ introCompilers/parsing/submission.py
@@ -0,0 +1,5 @@
+[DEFAULT]
+name = Soren Rasmussen
+host = ix.cs.uoregon.edu
+path = /home/users/srasmus2/studious-LCW/introCompilers/parsing
+repo = https://github.com/scrasmussen/studious-LCW/tree/master/introCompilers
