725d45d63fac9e78eeec2af61c10948b3b4fae5f
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-02-03 20:08:09 -0800

    Removed

--- introCompilers/submission.py
+++
@@ -1,5 +0,0 @@
-[DEFAULT]
-name = Soren Rasmussen
-host = ix.cs.uoregon.edu
-path = /home/users/srasmus2/studious-LCW/introCompilers
-repo = https://github.com/scrasmussen/studious-LCW/tree/master/introCompilers
