538eabc7a91b153ad9f2cb5e480e5a44223d3c73
Mohammad Alaul Haque Monil <monil01@gmail.com>
2017-02-14 16:17:20 -0800

    tree done

--- introCompilers/AST/Node.cpp
+++ introCompilers/AST/Node.cpp
@@ -3,103 +3,60 @@
 #include <vector>
 #include "Node.h"
 
-void checkStatementBlock(statementBlockNode *n, std::vector<char const*> *classNames) {
-
-  /* ===== TODO, VOID STATEMENT NOT ENTERING ===== */
-  //std::cout<<"NOTNULL\n";
-    // statementsNode *a=(statementsNode *) n->statements;
-      // if (!n->statements) 
-	// std::cout<<"NOTNULL\n";
+void checkConstructorClass(const char* name, std::vector<char const*> *classNames)
+{
+  int found = 0;
+  for (char const * a : *classNames) {
+    if (strcmp(name,a)==0) {
+      found = 1;
+    }
+  }
+  if (!found)
+    std::cerr << "Constructor Class \""<< name<<"\" Not Defined" << std::endl;
+ 
 }
 
-  void checkConstructor(constructorNode *n,  std::vector<char const*> *classNames)
-  {
-  //std::cout<<"CHECKING\n";
-    if (n !=NULL || n->name!=NULL) {
-      // std::cout << "NAME is not NULL  " << "\n";
-	//	std::cout << "NAME is not NULL  " << n->name << "\n";
-    }
-  else {
-    //std::cout<<"NULLL NAME +++++++\n";
-      }
-	     //std::cout<<"DONE CHECKING\n";
-    int found = 0;
-      //for (char const * a : *classNames) {
-      // if (strcpy(n->name,a)==0) {
-      // 	found = 1;
-      // std::cout << a << std::endl;
-      // 	}
-      //}
-
-      //if (!found)
-      // std::cerr << "Constructor Class Not Defined" << std::endl;"
-  }
-	     
 void checkRExpr(rExprNode *n, std::vector<char const*> *classNames)
 {
-  // const char* str = "";
-  // rExprNode *rExprFirst ;
-  // rExprNode *rExprSecond ;
-  // constructorNode *constructor ;
+  if ( strcmp(n->str,"const") == 0) 
+    checkConstructorClass(n->name, classNames);
   
-  if ( n->str="const"){
-    //std::cout << n->name <<"REXPR\n";
-    int found = 0;
-      for (char const * a : *classNames) {
-       if (strcmp(n->name,a)==0) {
-              found = 1;
-              //std::cout << a << std::endl;
-       }
-      }
-    if (!found)
-       std::cerr << "Constructor Class \""<< n->name<<"\" Not Defined" << std::endl;
-  }
-    // if (n->rExprFirst!=NULL){
-    //   std::cout << "NULL+++REXPR\n";
-    //   checkRExpr(n->rExprFirst, classNames);
-    //   }
-  // if (n->rExprSecond!=NULL)
-  //if (n->constructor != NULL)
-   // checkConstructor(n->constructor, classNames);
-
+  if (n->rExprFirst!=NULL)
+    checkRExpr(n->rExprFirst, classNames);
+  if (n->rExprSecond!=NULL)
+    checkRExpr(n->rExprSecond, classNames);
+  if (n->lExpr!=NULL)
+    checkLExpr(n->lExpr, classNames);
 }
-  
+
+void checkLExpr(lExprNode *n, std::vector<char const*> *classNames)
+{
+  if (n->rExpr!=NULL)
+    checkRExpr(n->rExpr, classNames);
+}
+
+
+void checkStatementBlock(statementBlockNode *n, std::vector<char const*> *classNames)
+{
+  if (n->statements!=NULL)
+    for (statementNode s : n->statements->list)
+    	checkStatement(s, classNames);
+}
+
 void checkStatement(statementNode n, std::vector<char const*> *classNames)
 {
-  // for (char const * a : *classNames)
-  //   std::cout << a << std::endl;
-
-  // ====Statement Struct====
-  // int value;
-  // const char* str;
-  // rExprNode* rExpr;
-  // statementBlockNode* stblock;
   if (n.rExpr!=NULL)
     checkRExpr(n.rExpr, classNames);
-/*jjjjj if (n.stblock!=NULL && n.stblock->statements!=NULL)
-    //checkStatementBlock(n.stblock, classNames);
-    for (statementNode s : n.stblock->statements->list)
-      {
-	checkStatement(s, classNames);
-      }*/ 
-
+  if (n.lExpr!=NULL)
+    checkLExpr(n.lExpr, classNames);
+  if (n.stblock!=NULL)
+    checkStatementBlock(n.stblock, classNames);
 }
 
 void checkMethod(methodNode  n, std::vector<char const*> *classNames)
-{
-  if (n.statementBlock != NULL && n.statementBlock->statements!=NULL)
-
-    for (statementNode s : n.statementBlock->statements->list)
-      {
-	checkStatement(s, classNames);
-      }
-
-//  if (n->methods != NULL)
- //   for (methodsNode m : n->methods->list)
-  //    {
-//	checkMethods(m, classNames);
- //     }
-
+{  
+  if (n.statementBlock!=NULL)
+    checkStatementBlock(n.statementBlock, classNames);
 }
 
 
