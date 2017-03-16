c97c2afee417ee746a0097602cad9e36fd593aae
Soren Rasmussen <s.c.rasmussen@gmail.com>
2017-03-16 12:59:41 -0700

    Simple changes, adding generate call

--- introCompilers/AST/ast.cpp
+++ introCompilers/AST/ast.cpp
@@ -50,12 +50,11 @@ int main(int argc, char*argv[]) {
   traverse(CHECKMETHOD);
   for(int i=0;i<10;i++)   traverse(CHECKLCA); 
   traverse(CHECKOBJECT);
-  //traverse(PRINT);
+  // traverse(PRINT);
   // traverse(PRINTST);
   traverse(CHECKARGTYPE);
   traverse(CHECKRETURNTYPE);
   traverse(CHECKLOGIC);
-  traverse(PRINTST);
   // traverse(PRINTST);
-  //generate();
+  generate();
 }
